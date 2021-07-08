ec73ba12fd714f4d5477dcae61bc48131fc3c2dc
Sudip Shrestha <xthazudp@gmail.com>
2021-07-08 19:56:38 +0545

    user create and update

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,7 +1,20 @@
 const admin = require('./../firebase');
 
-function authCheck(req, res, next) {
-  console.log(req.headers);
+async function authCheck(req, res, next) {
+  let token;
+  if (req.headers['authorization']) token = req.headers['authorization'];
+  if (req.headers['x-access-token']) token = req.headers['x-access-token'];
+  if (req.query['token']) token = req.query['token'];
+  if (!token) {
+    return next({
+      msg: 'Authentication Failed,Token not provided',
+      status: 401,
+    });
+  }
+  console.log('token=>', token);
+  const firebaseUser = await admin.auth().verifyIdToken(token);
+  console.log('User is=>', firebaseUser);
+  req.user = firebaseUser;
   next();
 }
 
--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -1,7 +1,20 @@
-function createOrUpdateUser(req, res) {
-  res.json({
-    data: 'You can create or update user.',
-  });
+const User = require('./../user/user.model');
+
+async function createOrUpdateUser(req, res) {
+  const { name, picture, email } = req.user;
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name, picture },
+    { new: true }
+  );
+  if (user) {
+    console.log('user updated', user);
+    res.json(user);
+  } else {
+    const newUser = await new User({ email, name, picture }).save();
+    console.log('user created', newUser);
+    res.json(newUser);
+  }
 }
 
 module.exports = {
